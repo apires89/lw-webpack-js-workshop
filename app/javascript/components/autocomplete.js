ce1824bfbcddb2b987acb972664d5f7e2c487d6c
Andre Pires <andrecabralpires@gmail.com>
2018-12-01 13:07:26 +0000

    autocomplete

--- app/javascript/components/autocomplete.js
+++ app/javascript/components/autocomplete.js
@@ -1,1 +1,23 @@
-autocomplete.js
+import autoComplete from 'js-autocomplete';
+
+const searchElement = document.getElementById('search-data');
+const skills = JSON.parse(searchElement.dataset.skills);
+
+const autoCompleteJs = function () {
+    new autoComplete({
+    selector: document.getElementById("q"),
+    minChars: 1,
+    source: function(term, suggest){
+        term = term.toLowerCase();
+        var choices = skills;
+        var matches = [];
+        for (var i=0; i<choices.length; i++)
+            if (~choices[i].toLowerCase().indexOf(term)) matches.push(choices[i]);
+        suggest(matches);
+    }
+});
+}
+
+
+export {autoCompleteJs};
+
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,12 @@
+import './application.css';
 import 'bootstrap';
 import {loadDynamicBannerText} from '../components/banner';
 import {initNavbarOnScrool} from '../components/navbar'
+import {autoCompleteJs} from '../components/autocomplete'
+
 
 loadDynamicBannerText();
 initNavbarOnScrool();
+autoCompleteJs();
+
+
